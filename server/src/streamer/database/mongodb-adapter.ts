b4c6f328a7d693da4c77e602f2aba652c3edf873
Dwayne Charrington <dwaynecharrington@gmail.com>
2023-05-11 13:32:12 +1000

    Create mongodb-adapter.ts

---
+++ server/src/streamer/database/mongodb-adapter.ts
@@ -0,0 +1,35 @@
+import { MongoClient } from "mongodb";
+import { DatabaseAdapter } from "./adapter";
+
+export class MongoDBAdapter implements DatabaseAdapter {
+  private client: MongoClient;
+  private dbName: string;
+  private db: any;
+
+  constructor(mongoUri: string, dbName: string) {
+    this.client = new MongoClient(mongoUri);
+    this.dbName = dbName;
+  }
+
+  async connect() {
+    await this.client.connect();
+    this.db = this.client.db(this.dbName);
+  }
+
+  async disconnect() {
+    await this.client.close();
+  }
+
+  async saveTransaction(transaction: any) {
+    await this.db.collection("transactions").insertOne(transaction);
+  }
+
+  async getLatestBlockNumber() {
+    const latestBlock = await this.db.collection("blocks").findOne({}, { sort: { blockNumber: -1 } });
+    return latestBlock ? latestBlock.blockNumber : 0;
+  }
+
+  async updateLatestBlockNumber(blockNumber: number) {
+    await this.db.collection("blocks").updateOne({}, { $set: { blockNumber } }, { upsert: true });
+  }
+}
