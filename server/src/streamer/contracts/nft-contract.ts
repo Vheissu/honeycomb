1308edf3f58fe544e7caab8b5aa1487cfcb0078e
Dwayne Charrington <dwaynecharrington@gmail.com>
2023-05-11 13:46:01 +1000

    Update nft-contract.ts

--- server/src/streamer/contracts/nft-contract.ts
+++ server/src/streamer/contracts/nft-contract.ts
@@ -3,25 +3,46 @@ import { DatabaseAdapter } from "./adapter";
 import { TableSchema } from "../database/table-schema";
 
 export class NFTContract implements Contract {
-  id = "nft";
-  name = "NFT";
+    id = "nft";
+    name = "NFT";
 
-  private nftTableSchema: TableSchema = {
-    tableName: "nfts",
-    columns: [
-      { name: "id", type: "TEXT", primaryKey: true },
-      { name: "owner", type: "TEXT" },
-      { name: "data", type: "TEXT" },
-    ],
-  };
+    private nftTableSchema: TableSchema = {
+        tableName: "nfts",
+        columns: [
+            { name: "id", type: "TEXT", primaryKey: true },
+            { name: "owner", type: "TEXT" },
+            { name: "data", type: "TEXT" },
+        ],
+    };
 
-  constructor(private dbAdapter: DatabaseAdapter) {
-    this.initialize();
-  }
+    constructor(private dbAdapter: DatabaseAdapter) {
+        this.initialize();
+    }
 
-  async initialize() {
-    await this.dbAdapter.createTable(this.nftTableSchema);
-  }
+    async initialize() {
+        await this.dbAdapter.createTable(this.nftTableSchema);
+    }
 
-  // ... rest of the NFTContract class
+    async execute(payload: any, context: any): Promise<void> {
+        const { action, nftId, data } = payload;
+
+        switch (action) {
+          case "create":
+            await this.createNFT(nftId, context.sender, data);
+            break;
+          case "update":
+            await this.updateNFT(nftId, data, context.sender);
+            break;
+          default:
+            console.error("Invalid action for NFT contract");
+        }
+    }
+
+    async createNFT(nftId: string, owner: string, data: any) {
+        await this.dbAdapter.updateTable("nfts", { id: nftId, owner, data: JSON.stringify(data) }, { id: nftId });
+    }
+
+    async updateNFT(nftId: string, data: any, sender: string) {
+        await this.dbAdapter.updateTable("nfts", { data: JSON.stringify(data) }, { id: nftId, owner: sender });
+    }
 }
