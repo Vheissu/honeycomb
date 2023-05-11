ea07c69d6cbfddb47aa2bdc8297eb26e6a9efdc6
Dwayne Charrington <dwaynecharrington@gmail.com>
2023-05-11 14:08:01 +1000

    Update table-schema.ts

--- server/src/streamer/database/table-schema.ts
+++ server/src/streamer/database/table-schema.ts
@@ -9,7 +9,8 @@ export interface TableSchema {
   columns: ColumnSchema[];
 }
 
-export function jsonSchemaToSql(schema: TableSchema): string {
+export function jsonSchemaToSqlite(schema: TableSchema): string {
+  // SQLite specific conversion logic
   const columnsSql = schema.columns.map(column => {
     let columnSql = `${column.name} ${column.type}`;
     if (column.primaryKey) {
@@ -19,4 +20,42 @@ export function jsonSchemaToSql(schema: TableSchema): string {
   }).join(', ');
 
   return `CREATE TABLE IF NOT EXISTS ${schema.tableName} (${columnsSql});`;
+}
+
+export function jsonSchemaToMysql(schema: TableSchema): string {
+  const columnsSql = schema.columns.map(column => {
+    let columnSql = `${column.name} ${column.type}`;
+    if (column.primaryKey) {
+      columnSql += " PRIMARY KEY";
+    }
+    return columnSql;
+  }).join(', ');
+
+  return `CREATE TABLE IF NOT EXISTS ${schema.tableName} (${columnsSql}) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;`;
+}
+
+export function jsonSchemaToPostgresql(schema: TableSchema): string {
+  const columnsSql = schema.columns.map(column => {
+    let columnSql = `${column.name} ${column.type}`;
+    if (column.primaryKey) {
+      columnSql += " PRIMARY KEY";
+    }
+    return columnSql;
+  }).join(', ');
+
+  return `CREATE TABLE IF NOT EXISTS ${schema.tableName} (${columnsSql});`;
+}
+
+export function jsonSchemaToMssql(schema: TableSchema): string {
+  const columnsSql = schema.columns.map(column => {
+    let columnSql = `${column.name} ${column.type}`;
+    if (column.primaryKey) {
+      columnSql += " PRIMARY KEY";
+    }
+    return columnSql;
+  }).join(', ');
+
+  return `IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='${schema.tableName}' AND xtype='U')
+          CREATE TABLE ${schema.tableName} (${columnsSql});
+          `;
 }
