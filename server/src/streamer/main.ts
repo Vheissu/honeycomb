f472ab5eb689e72ec2b924c5a1f5dad827393321
Dwayne Charrington <dwaynecharrington@gmail.com>
2023-05-11 13:26:40 +1000

    Create main.ts

---
+++ server/src/streamer/main.ts
@@ -0,0 +1,25 @@
+import { Client } from "@hiveio/dhive";
+import { HiveStreamer } from "./hive-streamer";
+import { MongoDBAdapter } from "./database/mongo-adapter";
+import { SQLiteAdapter } from "./database/sqlite-adapter";
+import { CoinflipContract } from "./contracts/coinflip";
+
+async function main() {
+  const client = new Client("https://api.hive.blog");
+  const dbAdapter = new MongoDBAdapter(/* MongoDB connection config */); // or new SQLiteAdapter(/* SQLite connection config */);
+
+  await dbAdapter.connect();
+
+  const hiveStreamer = new HiveStreamer(client, dbAdapter);
+
+  // Register the coinflip contract
+  const coinflipContract = new CoinflipContract();
+  hiveStreamer.registerContract(coinflipContract);
+
+  hiveStreamer.startStreaming();
+
+  // To stop streaming after some time, you can use:
+  // setTimeout(() => hiveStreamer.stopStreaming(), 10000);
+}
+
+main();
